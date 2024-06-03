6630ec489bc0e8243f578a60b54f8e2510632cf9
daanila01 <daanila01@gmail.com>
2024-06-03 09:14:27 +0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import {AppModule} from './app.module'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.use(cookieParser())
+    app.enableCors({
+        credentials: true,
+        origin: ['localhost:3000']
+    })
 
     await app.listen(3000)
 }
